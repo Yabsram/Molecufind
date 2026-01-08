75ef0bbf14d11db33c9fde971d5bcdbaf2841210
Nathalie Villa <njv2@njit.edu>
2026-01-08 21:47:04 +0000

    added routes and functions

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+molecule_db = load_molecule_database("original.csv")
 
 @app.route("/", methods=["GET"])
-def root():
-    return render_template("base.html")
+def select_property():
+    #show the form to select properties
+
+@app.route("/results", methods=["POST"])
+def show_results():
+    #display top results 
