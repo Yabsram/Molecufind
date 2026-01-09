461930d3d638fbe92740595a29f6b09e6734e837
Yabsra Maelaf <ymaelaf@gmail.com>
2026-01-09 01:16:06 +0000

    initial base html saving before leaving

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-molecule_db = load_molecule_database("original.csv")
+#molecule_db = load_molecule_database("original.csv")
 
 @app.route("/", methods=["GET"])
 def select_property():
-    #show the form to select properties
-
+    return render_template(
+        "selection.html"
+    )
 @app.route("/results", methods=["POST"])
 def show_results():
-    #display top results 
+   return render_template(
+        "base.html"
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8004)))
