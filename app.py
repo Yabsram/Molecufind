0a968373c1ea0ff75cc621bd7361a0a0a1fb4a34
Thuy Vy <tvytran2@gmail.com>
2026-01-09 00:46:24 +0000

    added method to take top 30 matching and returned the images

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template
+import pandas as pd, os, shutil
+from rdkit import Chem
+from rdkit.Chem import Draw
+import chemistry
 
 app = Flask(__name__)
 
-molecule_db = load_molecule_database("original.csv")
+#molecule_db = load_molecule_database("original.csv")
 
 @app.route("/", methods=["GET"])
 def select_property():
+    return
     #show the form to select properties
 
 @app.route("/results", methods=["POST"])
 def show_results():
-    #display top results 
+    return
+    #display top results
+
+if __name__ == '__main__':
+    d = {
+    'col1': ["Paracetamol", "CC(=O)NC1=CC=C(C=C1)O", "two"],
+    'col2': ["Nicotine", "CN1CCCC1C2=CN=CC=C2", "two"],
+    'col3': ["Aspirin", "CC(=O)OC1=CC=CC=C1C(=O)O", "three"],
+    'col4': ["Caffeine", "CN1C=NC2=C1C(=O)N(C(=O)N2C)C", "three"],
+    'col5': ["Morphine", "CN1CCC23C4C1CC5=C2C(=C(C=C5)O)OC3C(C=C4)O", "five"],
+    'col6': ["Ibuprofen", "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O", "four"],
+    'col7': ["Dopamine", "NCCC1=CC=C(O)C(O)=C1", "two"]
+    }
+    df = pd.DataFrame(data=d)
+    chemistry.show_images(df)
+    
--- chemistry.py
+++ chemistry.py
@@ -1,6 +1,8 @@
 from rdkit import Chem
 from rdkit.Chem import Draw
 from rdkit.DataStructs.cDataStructs import ExplicitBitVect
+import os, shutil
+import pandas as pd
 
 # Data loading & preparation (run once at module load or server startup)
 def load_molecule_database(csv_path: str) -> pd.DataFrame:
@@ -28,3 +30,29 @@ def get_top_similar_molecules(
     # Apply calculate_similarity_score to each row
     # Sort by score
     # Return top N
+
+def show_images(dataframe):
+    folder_path = "images"
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+        os.makedirs(folder_path)
+    else:
+        os.makedirs(folder_path)
+
+    #d = {'col1': ["Paracetamol", "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O", "two"], 'col2': ["Nicotine", "CN1CCCC1C2=CN=CC=C2", "two"]}
+    #df = pd.DataFrame(data=d)
+    df = dataframe
+
+    # Second column (index 1)
+    for col in df.columns:
+        name = df[col][0] #find out what index the name is stored at
+        structure = df[col][1] #find out what index the structure is stored at
+
+        mol = Chem.MolFromSmiles(structure)
+
+        # Save to file
+        filename = name + ".png"
+        img = Draw.MolToImage(mol)
+        filepath = os.path.join(folder_path, filename)
+        img.save(filepath)       
+    return
